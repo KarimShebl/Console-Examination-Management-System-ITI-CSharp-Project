e9ade4a9cbe1816b3c2cf254bc30647010e2e968
Karim Shebl <karimshebl003@gmail.com>
2026-03-06 22:44:22 +0200

    Added input validations to FinalExam.

--- Examination Management System/Exams/FinalExam.cs
+++ Examination Management System/Exams/FinalExam.cs
@@ -39,13 +39,38 @@ namespace Examination_Management_System.Exams
             {
                 q.Display();
 
-                Console.Write("Enter Answer Id: ");
-                string input = Console.ReadLine();
-                Console.WriteLine("\n\n\n");
+                int[] numbers;
 
-                int[] numbers = input.Split(' ')
-                                     .Select(x => int.Parse(x))
-                                     .ToArray();
+                while (true)
+                {
+                    Console.WriteLine("Enter numbers separated by spaces:");
+
+                    string input = Console.ReadLine();
+                    string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    bool valid = true;
+                    List<int> temp = new List<int>();
+
+                    foreach (var p in parts)
+                    {
+                        if (int.TryParse(p, out int num) && num >= 1 && num <= q.Answers.Count)
+                        {
+                            temp.Add(num);
+                        }
+                        else
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if (valid)
+                    {
+                        numbers = temp.ToArray();
+                        break;
+                    }
+
+                    Console.WriteLine($"Invalid input. Please enter only integers between 1 and {q.Answers.Count} separated by spaces.");
+                }
 
                 List<Answer> ans = new List<Answer>();
                 foreach (int id in numbers)
